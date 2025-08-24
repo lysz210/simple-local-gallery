24168baf7f605e407df72de0d0dc7299fb6ad32f
Lingyong Sun <lingyong.sun@gmail.com>
2025-08-24 22:43:25 +0200

    update models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from typing import Optional
 from datetime import datetime
 from sqlalchemy import (
     ForeignKey,
-    DateTime
+    DateTime,
+    UniqueConstraint
 )
 from sqlalchemy.sql import (
     func
@@ -21,20 +22,25 @@ class Photo(Base):
     __tablename__ = "photos"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    filename: Mapped[str] = mapped_column()
+    filename: Mapped[str] = mapped_column(unique=True)
     description: Mapped[str] = mapped_column()
     original_created_at: Mapped[datetime] = mapped_column()
 
-    gps_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"))
+    gps_point_id: Mapped[Optional[int]] = mapped_column(ForeignKey("points.id"))
     gps_point: Mapped[Optional["GpsPoint"]] = relationship(back_populates="photos")
 
 class GpsPoint(Base):
     __tablename__ = "points"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    track: Mapped[str] = mapped_column()
     latitude: Mapped[float] = mapped_column()
     longitude: Mapped[float] = mapped_column()
     elevation: Mapped[Optional[float]] = mapped_column()
-    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=False), server_default=func.now())
+    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=False), server_default=func.utcnow())
 
     photos: Mapped[Optional[list["Photo"]]] = relationship(back_populates="gps_point")
+    
+    __table_args__ = (
+        UniqueConstraint('track', 'timestamp', name='_gps_unique_track_point'),
+    )
