d6e3f5396d4244d31b875e69b1c638fa9b7f5a4e
Lingyong Sun <lingyong.sun@gmail.com>
2025-09-03 20:30:11 +0200

    update GalleryDrawer

--- src/components/GalleryDrawer.vue
+++ src/components/GalleryDrawer.vue
@@ -1,27 +1,53 @@
 <template>
 <v-navigation-drawer app>
-    <v-list nav v-if="!photosSummaries.error">
-      <v-list-item
-      v-for="summary in photosSummaries.data" :key="summary.folder"
-      :title="`${summary.folder} with ${summary.total_photos} photos`"
-      ></v-list-item>
-    </v-list>
+    <v-list nav>
+      <v-list-group value="Photos Summaries" v-if="!photosSummaries.error">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Photos Summaries"
+            prepend-icon="mdi-image-album"
+          ></v-list-item>
+        </template>
+        <v-list-item
+          v-for="summary in photosSummaries.data" :key="summary.folder"
+          :title="`${summary.folder}`"
+          :subtitle="`with ${summary.total_photos} photos`"
+        ></v-list-item>
+      </v-list-group>
 
-    <v-list nav v-if="!tracksSummaries.error">
-      <v-list-item
-      v-for="summary in tracksSummaries.data" :key="summary.uid"
-      :title="`${summary.uid} ${summary.name} with ${summary.total_points} points`"
-      ></v-list-item>
-    </v-list>
+      <v-list-group value="Tracks Summaries" v-if="!tracksSummaries.error">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Tracks Summaries"
+            prepend-icon="mdi-map-marker-path"
+          ></v-list-item>
+        </template>
+        <v-list-item
+          v-for="summary in tracksSummaries.data" :key="summary.uid"
+          :title="`track[${summary.uid}] with ${summary.total_points} points`"
+          :subtitle="`${summary.name}`"
+        ></v-list-item>
+      </v-list-group>
 
-    <v-list nav v-if="!fsSummaries.error">
-      <v-list-item v-if="fsSummaries.data?.gpx_files_count"
-      :title="`Found ${fsSummaries.data.gpx_files_count} GPX files`"
-      ></v-list-item>
-      <v-list-item
-      v-for="summary in fsSummaries.data?.folders" :key="summary.folder"
-      :title="`${summary.folder} with ${summary.total_photos} photos`"
-      ></v-list-item>
+      <v-list-group value="Filesystem Summaries" v-if="!fsSummaries.error">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Filesystem Summaries"
+            prepend-icon="mdi-file-tree"
+          ></v-list-item>
+        </template>
+        <v-list-item v-if="fsSummaries.data?.gpx_files_count"
+        :title="`Found ${fsSummaries.data.gpx_files_count} GPX files`"
+        ></v-list-item>
+        <v-list-item
+          v-for="summary in fsSummaries.data?.folders" :key="summary.folder"
+          :title="`${summary.folder}`"
+          :subtitle="`with ${summary.total_photos} photos`"
+        ></v-list-item>
+      </v-list-group>
     </v-list>
 </v-navigation-drawer>
 </template>
