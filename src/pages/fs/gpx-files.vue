75220e6f88e892d3be54c0e48f7e97cef2c075d5
Lingyong Sun <lingyong.sun@gmail.com>
2025-09-07 18:36:00 +0200

    add import button

--- src/pages/fs/gpx-files.vue
+++ src/pages/fs/gpx-files.vue
@@ -5,9 +5,15 @@
             :items="gpxFiles.data"
             v-model="selectedGpxFile"
         ></v-autocomplete>
-        <p>{{ gpxData?.uid }}  {{ gpxData?.name }}</p>
-        <p>{{ gpxData?.timestamp }}</p>
-        <p>{{ gpxData?.points?.length }}</p>
+        <v-btn
+            v-if="selectedGpxFile"
+            @click="importGpx(selectedGpxFile)"
+            prepend-icon="mdi-tray-arrow-up"
+            append-icon="mdi-tray-arrow-up"
+            block
+        >
+            Import {{ gpxData?.uid }}  {{ gpxData?.name }} - {{ gpxData?.timestamp }} with {{ gpxData?.points?.length }} points
+        </v-btn>
         <TrackMap :gpxData="gpxData" v-if="!gpxError && gpxData"></TrackMap>
     </v-container>
 </template>
@@ -18,9 +24,10 @@ import { findGpxFilesQuery } from '@/slg-api/@pinia/colada/filesystem.gen';
 import { inspectGpxFileQueryKey } from '@/slg-api/@pinia/colada/tracks.gen';
 import { Tracks, type Options } from '@/slg-api/sdk.gen';
 import type { InspectGpxFileData } from '@/slg-api/types.gen';
-import { defineQueryOptions, useQuery } from '@pinia/colada';
+import { defineQueryOptions, useQuery, useQueryCache } from '@pinia/colada';
 
 const router = useRouter()
+const queryCache = useQueryCache()
 
 const selectedGpxFile = ref(null)
 
@@ -51,6 +58,23 @@ const gpxInpectQuery = defineQueryOptions((gpx_file: string|null) => {
 })
 
 const { data: gpxData, error: gpxError } = useQuery(gpxInpectQuery, () => selectedGpxFile.value)
+
+async function importGpx(gpxFile: string) {
+    try {
+        const { data: trackUid } = await Tracks.importPgxFile({
+            path: {
+                gpx_file: gpxFile
+            }
+        });
+        console.info("Imported gpxFile", trackUid)
+        queryCache.invalidateQueries({
+            key: [{tags: ["tracks"]}]
+        })
+    } catch (error) {
+        console.error("Error importing gpxFile", gpxFile, error);
+    }
+}
+
 watch(gpxError, async (newError) => {
     if (newError) {
         console.error(newError)
