c326a5e43b6d66ea8eb122160855fdba943c4854
Lingyong Sun <lingyong.sun@gmail.com>
2025-09-11 21:46:27 +0200

    add stored tracks bounds in map

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,13 +8,60 @@
         name="OpenStreetMap"
         :attribution="attribution"
       ></l-tile-layer>
+      <l-rectangle
+        v-for="(bounds, index) in trackBounds" :key="index"
+        :bounds="[[bounds.min.latitude, bounds.min.longitude], [bounds.max.latitude, bounds.max.longitude]]"
+        color="blue"
+      ></l-rectangle>
     </l-map>
   </v-sheet>
 </template>
 
 <script lang="ts" setup>
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { useTraksSummaries } from '@/stores/traks'
+import { latLng, latLngBounds } from 'leaflet'
+import { LMap, LTileLayer, LRectangle } from '@vue-leaflet/vue-leaflet'
 
 const zoom = 6
 const attribution=`&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`
+
+const mapRef = useTemplateRef('map')
+const { data: tracksSummaries } = useTraksSummaries()
+
+const trackBounds = computed(() => {
+  if (!tracksSummaries.value?.length) {
+    return []
+  }
+  return tracksSummaries.value
+    .map(track => track.bounds)
+    .filter(bounds => !!bounds)
+})
+
+watchEffect(() => {
+  if (tracksSummaries.value?.length) {
+    const boundsValues = tracksSummaries.value
+      .map(track => track.bounds)
+      .filter(bounds => !!bounds)
+      .reduce((acc, bounds) => {
+        acc.minLat.push(bounds.min.latitude);
+        acc.minLng.push(bounds.min.longitude);
+        acc.maxLat.push(bounds.max.latitude);
+        acc.maxLng.push(bounds.max.longitude);
+        return acc;
+      }, {
+        minLat: [] as number[],
+        minLng: [] as number[],
+        maxLat: [] as number[],
+        maxLng: [] as number[]
+      })
+    if (boundsValues.minLat && boundsValues.minLng && boundsValues.maxLat && boundsValues.maxLng) {
+      const bounds = latLngBounds(
+        latLng(Math.min(...boundsValues.minLat), Math.min(...boundsValues.minLng)),
+        latLng(Math.max(...boundsValues.maxLat), Math.max(...boundsValues.maxLng))
+      )
+      // @ts-ignore
+      mapRef.value?.leafletObject.fitBounds(bounds);
+    }
+  }
+})
 </script>
