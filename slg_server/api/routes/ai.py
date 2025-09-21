5fd643c282f978e3ea099faa62b47b159bce2ce0
Lingyong Sun <lingyong.sun@gmail.com>
2025-09-21 10:46:37 +0200

    udpate

--- slg_server/api/routes/ai.py
+++ slg_server/api/routes/ai.py
@@ -5,6 +5,6 @@ from ...services import gemini
 
 router = APIRouter(prefix='/ai', tags=['ai'])
 
-@router.get("inspect-photo", name="Inspect photo with GenAI", operation_id="inpect_photo")
+@router.get("/inspect-photo", name="Inspect photo with GenAI", operation_id="inpect_photo")
 async def inpect_photo(id: int) -> dto.PhotoInfo:
     return gemini.inspect_photo(id)
