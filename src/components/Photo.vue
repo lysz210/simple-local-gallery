40d5238771eb712c80b8ee10a4495fec4b7677b0
Lingyong Sun <lingyong.sun@gmail.com>
2025-10-04 21:58:57 +0200

    display address info

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -20,38 +20,17 @@ class="text-white"
     </v-card-subtitle>
     <v-card-subtitle class="text-white" v-if="point">
         LAT: {{ point.latitude }} LNG: {{ point.longitude }} TRACK {{ point.track_uid }}
+        <span v-if="point.address">{{ point.address.display_name }}</span>
     </v-card-subtitle>
     <v-card-text class="text-white">{{ currentPhoto.description }}</v-card-text>
     </v-img>
 
-    <v-card-actions>
+    <v-card-actions class="text-black">
         <v-btn v-if="!point"
         v-for="(point, i) in suggetedGpsPoints" :kye="i"
         @click="updatePhotoPoint(point)"
         >{{ point }}</v-btn>
-        <v-btn v-if="!aiInfo"
-        @click="inspectPhoto(photo)"
-        >Inspect photo with AI</v-btn>
-        
     </v-card-actions>
-    <v-expand-transition>
-        <v-card-text v-if="aiInfo"
-        >
-        <v-textarea
-        label="Ai description"
-        v-model="newDescription"
-        ></v-textarea>
-        
-        <v-chip-group
-        v-model="selectedAiTags"
-        multiple column
-        >
-            <v-chip v-for="tag in aiInfo.tags" :value="tag" filter>{{ tag }}</v-chip>
-        </v-chip-group>
-        {{ selectedAiTags }}
-        <v-btn @click="patchPhoto({ description: newDescription, tags: selectedAiTags })">Update photo infos</v-btn>
-        </v-card-text>
-    </v-expand-transition>
 </v-card>
 </template>
 
@@ -59,8 +38,8 @@ class="text-white"
 import AiHelper from './AiHelper.vue';
 import FlickrDialog from './FlickrDialog.vue';
 import { locatePhotoOnTrackQuery } from '@/slg-api/@pinia/colada/tracks.gen';
-import { Ai, Photos } from '@/slg-api/sdk.gen';
-import type { Photo, PhotoInfo, PhotoPatch, PointWithTrackUid } from '@/slg-api/types.gen';
+import { Photos } from '@/slg-api/sdk.gen';
+import type { Photo, PhotoInfo, PointWithTrackUid } from '@/slg-api/types.gen';
 import { useSocialStore } from '@/stores/social';
 import { useMutation, useQuery } from '@pinia/colada';
 
@@ -91,7 +70,6 @@ const { data: suggetedGpsPoints } = useQuery({
     enabled: !photo.point
 })
 
-
 const { mutate: updatePhotoPoint } = useMutation({
     mutation: async (point: PointWithTrackUid) => {
         const { data } = await Photos.updatePhotoPoint({
@@ -106,38 +84,6 @@ const { mutate: updatePhotoPoint } = useMutation({
             point.value = data.point
             emit("updateGpsPoint", data.point)
         }
-    }
-})
-
-const { mutate: inspectPhoto } = useMutation({
-    mutation: async (photo: Photo) => {
-        const { data } = await Ai.inpectPhoto({
-            query: { id: photo.id },
-            throwOnError: true
-        });
-        return data;
-    },
-    onSuccess: (data) => {
-        aiInfo.value = data;
-        newDescription.value = data.description;
-        selectedAiTags.value.push(...data.tags);
-    }
-})
-
-const { mutate: patchPhoto } = useMutation({
-    mutation: async (body: PhotoPatch) => {
-        const { data } = await Photos.patchPhoto({
-            path: { id: photo.id },
-            body,
-            throwOnError: true
-        });
-        return data;
-    },
-    onSuccess: (data) => {
-        currentPhoto.value = data;
-        aiInfo.value = null;
-        selectedAiTags.value = [];
-        newDescription.value = null;
     }
 })
 </script>
